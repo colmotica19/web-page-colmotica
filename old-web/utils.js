956644c57f736303992afc6f3d56bab211721f1d
juanzb <juanzarache2000@gmail.com>
2025-08-12 16:05:26 -0500

    remove foledr old-page

--- old-web/utils.js
+++
@@ -1,7 +0,0 @@
-import { NotFoundHTMLElement } from "./errors.js";
-export function querySelector(selectors, element) {
-    const item = (element ?? document).querySelector(selectors);
-    if (!item)
-        throw new NotFoundHTMLElement(selectors);
-    return item;
-}
