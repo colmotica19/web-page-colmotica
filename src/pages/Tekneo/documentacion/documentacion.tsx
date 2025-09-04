6b310435fc09b71bb8998d736acf0e2d20046641
Arnold Steven Beleño Zuletta <lordwesxt@gmail.com>
2025-09-04 17:32:01 -0500

    Add styles

--- src/pages/Tekneo/documentacion/documentacion.tsx
+++ src/pages/Tekneo/documentacion/documentacion.tsx
@@ -5,7 +5,7 @@ import Popover, { type PopoverHandle } from "../../../components/Popover/Popover
 
 export default function Documentacion() {
   const { t } = useTranslation()
-  const [viewProduct, setViewProduct] = useState<keyof typeof infoHardware>("Modulo TK-IO22W");
+  const [viewProduct, setViewProduct] = useState<keyof typeof infoHardware | keyof typeof infoSoftware>("Modulo TK-IO22W");
   const viewProductDescription = useRef<HTMLElement>(null);
   const [anchors, setAnchors] = useState<Record<string, JSX.Element[]>>();
 
@@ -74,26 +74,77 @@ export default function Documentacion() {
   }), [t])
 
   const infoSoftware = useMemo(() => ({
-    "Access Control": {
-      title: "Access Control",
-      subtitles: {
-        1: {
-          name: t("access_control_1"),
-          imgSrc: ["/img/access_control_1.jpg", "/img/access_control_2.png", "/img/access_control_3.png"],
-          description: {
-            1: t("access_control_1_2"),
-            2: t("access_control_1_3"),
-            3: t("access_control_1_4"),
-            4: t("access_control_1_5")
-          }
-        }
-      }
+    "Tgate": {
+      title: "TGate",
+      content: {
+        1: t("tgate_1"),
+        2: t("tgate_1_1"),
+        3: t("tgate_1_2"),
+        11: t("tgate_2"),
+        12: t("tgate_2_1"),
+        13: t("tgate_3"),
+        14: t("tgate_3_1"),
+        15: t("tgate_4"),
+        16: t("tgate_4_1"),
+        17: t("tgate_5"),
+        18: t("tgate_5_1"),
+        19: t("tgate_end")
+      },
+      list: {
+        4: t("tgate_1_list_1"),
+        5: t("tgate_1_list_2"),
+        6: t("tgate_1_list_2_1"),
+        7: t("tgate_1_list_2_2"),
+        8: t("tgate_1_list_2_3"),
+        9: t("tgate_1_list_2_4"),
+        10: t("tgate_1_list_3"),
+      },
+      imgSrc: ["/img/tgate_1.jpg", "/img/tgate_2.jpg", "/img/tgate_3.jpg", "/img/tgate_4.jpg", "/img/tgate_5.jpg"]
     }
-  }), [t])
+  } as const), [t])
+
+  function renderAccessControl() {
+    const data = infoSoftware[viewProduct as keyof typeof infoSoftware];
+    if (!data) return null;
+
+    return (
+      <div className="flex flex-col gap-[20px] justify-center items-start">
+        <h1 className="self-center text-[32px] text-center mb-[5px] font-bold title">{data.title}</h1>
+        <h2 className="font-bold text-[24px]" id="description" data-title-anchor="Introducción">{data.content[1]}</h2>
+        <img src={data.imgSrc[0]} alt="" width={"auto"} className="self-center w-auto h-auto" />
+        <p dangerouslySetInnerHTML={{ __html: data.content[2] }}></p>
+        <p>{data.content[3]}</p>
+        <ul className="flex flex-col gap-[0.5lh]">
+          <li dangerouslySetInnerHTML={{ __html: data.list[4] }} className="list-disc pl-[20px] ml-[15px]"></li>
+          <li dangerouslySetInnerHTML={{ __html: data.list[5] }} className="list-disc pl-[20px] ml-[15px]"></li>
+          <ul className="ml-[20px] flex flex-col gap-[0.5lh]">
+            <li dangerouslySetInnerHTML={{ __html: data.list[6] }} className="list-decimal pl-[20px] ml-[15px]"></li>
+            <li dangerouslySetInnerHTML={{ __html: data.list[7] }} className="list-decimal pl-[20px] ml-[15px]"></li>
+            <li dangerouslySetInnerHTML={{ __html: data.list[8] }} className="list-decimal pl-[20px] ml-[15px]"></li>
+            <li dangerouslySetInnerHTML={{ __html: data.list[9] }} className="list-decimal pl-[20px] ml-[15px]"></li>
+          </ul>
+          <li dangerouslySetInnerHTML={{ __html: data.list[5] }} className="list-disc pl-[20px] ml-[15px]"></li>
+        </ul>
+        <h1 className="font-bold text-[24px]" data-title-anchor="Camara" id="camara">{data.content[11]}</h1>
+        <img src={data.imgSrc[2]} alt="" width={"auto"} className="self-center w-auto h-auto" />
+        <p>{data.content[12]}</p>
+        <img src={data.imgSrc[3]} alt="" width={"auto"} className="self-center w-auto h-auto" />
+        <h1 className="font-bold text-[24px]">{data.content[13]}</h1>
+        <img src={data.imgSrc[1]} alt="" width={"auto"} className="self-center w-auto h-auto" />
+        <p>{data.content[14]}</p>
+        <h1 className="font-bold text-[24px]">{data.content[15]}</h1>
+        <img src={data.imgSrc[4]} alt="" width={"auto"} className="self-center w-auto h-auto" />
+        <p>{data.content[16]}</p>
+        <h1 className="font-bold text-[24px]">{data.content[17]}</h1>
+        <p>{data.content[18]}</p>
+        <p>{data.content[19]}</p>
+      </div>
+    )
+  }
 
   // Esta función inserta la info en el HTML
   function renderProductInfo() {
-    const data = infoHardware[viewProduct];
+    const data = infoHardware[viewProduct as keyof typeof infoHardware];
     if (!data) return null;
 
     return (
@@ -104,7 +155,7 @@ export default function Documentacion() {
           <p className="descriptionContent">{data.descriptionContent}</p>
           <ul className="listOfFeatures flex flex-col gap-[0.5lh]">
             {Object.values(data.listOfFeatures).map((feature, i) => (
-              <li className="list-disc pl-[1lh] ml-[10px] mb-[10px]" key={i}>{feature}</li>
+              <li className="list-disc pl-[20px] ml-[15px]" key={i}>{feature}</li>
             ))}
           </ul>
           <h1 className="font-bold text-[24px] text-black" id="aplicaciones" data-title-anchor="Aplicaciones">{t("aplicaciones")}</h1>
@@ -139,26 +190,40 @@ export default function Documentacion() {
       </>
     );
   }
-  function renderPreviewProductInfo(productName: keyof typeof infoHardware) {
-    const data = infoHardware[productName]
-    return (
-      <>
-        <h1>{data.title}</h1>
-        {data.diagramSrc.map((item, index) => (
-          <img src={item} alt="img" width={"auto"} key={index} className="h-auto w-[220px]" />
-        ))}
-      </>
-    )
+  function renderPreviewProductInfo(productName: keyof typeof infoHardware | keyof typeof infoSoftware) {
+    if (Object.keys(infoHardware).some((item) => item === productName)) {
+      const data = infoHardware[productName as keyof typeof infoHardware]
+      return (
+        <>
+          <h1>{data.title}</h1>
+          {data.diagramSrc.map((item, index) => (
+            <img src={item} alt="img" width={"auto"} key={index} className="h-auto w-[220px]" />
+          ))}
+        </>
+      )
+    } else if (Object.keys(infoSoftware).some((item) => item === productName)) {
+      const data = infoSoftware[productName as keyof typeof infoSoftware];
+
+      return (
+        <>
+          <h1>{data.title}</h1>
+          <img src={data.imgSrc[0]} alt="" className="w-[500px] h-auto" width={"auto"} height={"auto"} />
+        </>
+      )
+    }
   }
 
   const btnModule22w = useRef<HTMLButtonElement>(null)
   const btnModule44w = useRef<HTMLButtonElement>(null)
+  const btnTgate = useRef<HTMLButtonElement>(null);
   const popoverHandle = useRef<PopoverHandle>(null)
   const popoverHandle2 = useRef<PopoverHandle>(null)
+  const popoverAccessControlHandle = useRef<PopoverHandle>(null)
   btnModule22w.current?.addEventListener("mouseover", () => {
     if (btnModule22w.current) {
       popoverHandle.current?.showPopover(btnModule22w.current)
       popoverHandle2.current?.forceClose()
+      popoverAccessControlHandle.current?.forceClose()
     }
   })
   btnModule22w.current?.addEventListener("mouseleave", () => {
@@ -170,11 +235,24 @@ export default function Documentacion() {
     if (btnModule44w.current) {
       popoverHandle2.current?.showPopover(btnModule44w.current)
       popoverHandle.current?.forceClose()
+      popoverAccessControlHandle.current?.forceClose()
     }
   })
   btnModule44w.current?.addEventListener("mouseleave", () => {
     if (btnModule44w.current) {
       popoverHandle2.current?.close()
+    }
+  })
+  btnTgate.current?.addEventListener("mouseover", () => {
+    if (btnTgate.current) {
+      popoverAccessControlHandle.current?.showPopover(btnTgate.current)
+      popoverHandle2.current?.forceClose()
+      popoverHandle.current?.forceClose()
+    }
+  })
+  btnTgate.current?.addEventListener("mouseleave", () => {
+    if (btnTgate.current) {
+      popoverAccessControlHandle.current?.close()
     }
   })
   const listOfNav = useRef<HTMLUListElement>(null)
@@ -233,7 +311,7 @@ export default function Documentacion() {
   }, [viewProduct])
 
   return (
-    <article className="grid grid-cols-[200px_minmax(500px,45vw)_200px] gap-x-[8%] justify-center m-[50px_0px] relative">
+    <article className="grid grid-cols-[200px_minmax(500px,50vw)_200px] gap-x-[8%] justify-center m-[50px_0px] relative">
       <aside className="relative flex flex-col gap-[30px] items-center after:w-[2px] after:h-[30%] after:absolute after:top-[10px] after:left-[calc(100%_+_15px)] after:bg-gray-300">
         <div className="w-full">
           <h1 className="text-black font-bold text-[32px]">Hardware</h1>
@@ -243,9 +321,10 @@ export default function Documentacion() {
           <li>
             <button ref={btnModule22w} data-active="true" className={`flex gap-[10px] min-w-[200px] items-center justify-end p-[5px_15px] btnSection rounded-[8px] hover:text-white text-gray-300 ${viewProduct === "Modulo TK-IO22W" ? "!text-white !border-black" : ""}`} onClick={() => {
               setViewProduct("Modulo TK-IO22W")
-              if (btnModule22w.current && btnModule44w.current) {
+              if (btnModule22w.current && btnModule44w.current && btnTgate.current) {
                 btnModule22w.current.dataset.active = "true"
                 btnModule44w.current.dataset.active = "false"
+                btnTgate.current.dataset.active = "false"
               } else {
                 throw new Error("La referencia 'btnModule22w' o 'btnModule44w' es null o undefined")
               }
@@ -271,9 +350,10 @@ export default function Documentacion() {
           <li>
             <button ref={btnModule44w} className={`flex gap-[10px] min-w-[200px] items-center justify-end p-[5px_10px] btnSection rounded-[8px] hover:text-white text-gray-300 ${viewProduct === "Modulo TK-IO24W2" ? "!text-white !border-black" : ""}`} onClick={() => {
               setViewProduct("Modulo TK-IO24W2")
-              if (btnModule44w.current && btnModule22w.current) {
+              if (btnModule44w.current && btnModule22w.current && btnTgate.current) {
                 btnModule22w.current.dataset.active = "false"
                 btnModule44w.current.dataset.active = "true"
+                btnTgate.current.dataset.active = "false"
               } else {
                 throw new Error("La referencia 'btnModule22w' o 'btnModule44w' es null o undefined")
               }
@@ -302,32 +382,57 @@ export default function Documentacion() {
           <h1 className="text-black font-bold text-[32px]">Software</h1>
           <hr className="w-full border-t-gray-400" />
         </div>
-          <ul className="flex flex-col gap-[5px]">
-            <li>
-              <button type="button" className={`flex gap-[10px] min-w-[200px] items-center justify-center p-[5px_10px] btnSection rounded-[8px] hover:text-white text-gray-300 ${viewProduct === "Modulo TK-IO24W2" ? "!text-white !border-black" : ""}`}>
-                <span>Access Control</span>
-                <svg xmlns="http://www.w3.org/2000/svg" className="fill-white size-[16px] rotate-90" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 -4.5 20 20" version="1.1">
-                  <g id="Page-1" stroke="none" strokeWidth="1" fill="inherit" fillRule="evenodd">
-                    <g id="Dribbble-Light-Preview" transform="translate(-260.000000, -6684.000000)" fill="inherit">
-                      <g id="icons" transform="translate(56.000000, 160.000000)">
-                        <path d="M223.707692,6534.63378 L223.707692,6534.63378 C224.097436,6534.22888 224.097436,6533.57338 223.707692,6533.16951 L215.444127,6524.60657 C214.66364,6523.79781 213.397472,6523.79781 212.616986,6524.60657 L204.29246,6533.23165 C203.906714,6533.6324 203.901717,6534.27962 204.282467,6534.68555 C204.671211,6535.10081 205.31179,6535.10495 205.70653,6534.69695 L213.323521,6526.80297 C213.714264,6526.39807 214.346848,6526.39807 214.737591,6526.80297 L222.294621,6534.63378 C222.684365,6535.03868 223.317949,6535.03868 223.707692,6534.63378" id="arrow_up-[#337]">
-                        </path>
-                      </g>
+        <ul className="flex flex-col gap-[5px]">
+          <li>
+            <button onClick={() => {
+              setViewProduct("Tgate")
+              if (btnModule44w.current && btnModule22w.current && btnTgate.current) {
+                btnModule22w.current.dataset.active = "false"
+                btnModule44w.current.dataset.active = "false"
+                btnTgate.current.dataset.active = "true"
+              } else {
+                throw new Error("La referencia 'btnModule22w' o 'btnModule44w' es null o undefined")
+              }
+            }} ref={btnTgate} type="button" className={`flex gap-[10px] min-w-[200px] items-center justify-center p-[5px_10px] btnSection rounded-[8px] hover:text-white text-gray-300 ${viewProduct === "Modulo TK-IO24W2" ? "!text-white !border-black" : ""}`}>
+              <span>TGate</span>
+              <svg xmlns="http://www.w3.org/2000/svg" className="fill-white size-[16px] rotate-90" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 -4.5 20 20" version="1.1">
+                <g id="Page-1" stroke="none" strokeWidth="1" fill="inherit" fillRule="evenodd">
+                  <g id="Dribbble-Light-Preview" transform="translate(-260.000000, -6684.000000)" fill="inherit">
+                    <g id="icons" transform="translate(56.000000, 160.000000)">
+                      <path d="M223.707692,6534.63378 L223.707692,6534.63378 C224.097436,6534.22888 224.097436,6533.57338 223.707692,6533.16951 L215.444127,6524.60657 C214.66364,6523.79781 213.397472,6523.79781 212.616986,6524.60657 L204.29246,6533.23165 C203.906714,6533.6324 203.901717,6534.27962 204.282467,6534.68555 C204.671211,6535.10081 205.31179,6535.10495 205.70653,6534.69695 L213.323521,6526.80297 C213.714264,6526.39807 214.346848,6526.39807 214.737591,6526.80297 L222.294621,6534.63378 C222.684365,6535.03868 223.317949,6535.03868 223.707692,6534.63378" id="arrow_up-[#337]">
+                      </path>
                     </g>
                   </g>
-                </svg>
-              </button>
-            </li>
-          </ul>
+                </g>
+              </svg>
+            </button>
+            <Popover ref={popoverAccessControlHandle} gapTop={-(btnModule44w.current?.offsetHeight ?? 100)} gapLeft={(btnModule44w.current?.offsetWidth ?? 100) + 15}>
+              <div className="flex flex-col gap-[10px] items-center">
+                {renderPreviewProductInfo("Tgate")}
+              </div>
+            </Popover>
+          </li>
+        </ul>
       </aside>
       <section ref={viewProductDescription} className="flex flex-col gap-[1lh]">
-        {renderProductInfo()}
+        {renderProductInfo() ?? renderAccessControl()}
       </section>
-      <aside className="sticky top-[150px] self-start">
-        <h1 className="text-[18px]">{viewProduct}</h1>
-        <ul className={`listOfNav mt-[10px] flex-col items-start gap-[5px]`} ref={listOfNav}>
-          {anchors ? anchors[viewProduct] : ""}
-        </ul>
+      <aside className="sticky top-[125px] self-start flex flex-col items-center gap-[20px]">
+        <div>
+          <h1 className="text-[18px]">{t("tabla_de_contenido")}</h1>
+          <ul className={`listOfNav mt-[10px] flex-col items-start gap-[5px] min-h-[150px]`} ref={listOfNav}>
+            {anchors ? anchors[viewProduct] : ""}
+          </ul>
+        </div>
+        <hr className="border-t-gray-400 w-full" />
+        <a href="/docs/Manual - Tekneo Software.pdf" className="flex p-[5px_15px] justify-center gap-[20px] size-full btnDownload rounded-[10px] w-full" download={true} type="button">
+          <span>{t("manual")}</span>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="inherit" className="size-[24px]">
+            <g id="Interface / Download">
+              <path id="Vector" d="M6 21H18M12 3V17M12 17L17 12M12 17L7 12" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </g>
+          </svg>
+        </a>
       </aside>
     </article>
   )
